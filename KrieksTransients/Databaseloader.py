f7e45884453524ac7b448fee68ec2e16b9c73cd6
Kriandir <kriekvdmeulen@gmail.com>
2018-06-10 15:03:17 +0200

    initial commit

---
+++ KrieksTransients/Databaseloader.py
@@ -0,0 +1,43 @@
+import numpy as np
+import pandas as pd
+
+# tiny class wrapper around the tgssdata for ease of access
+class datawrapp(object):
+    def __init__(self,name,ra,rae,dec,dece,pflux,pfluxe,iflux,ifluxe):
+
+        self.radec = np.stack((ra,dec),axis = -1)
+        self.ra = ra
+	if name == "tgss":
+            self.rae = rae/3600
+        else:
+            self.rae = rae
+        self.dec = dec
+        if name == "tgss":
+            self.dece = dece/3600
+        else:
+            self.dece = dece
+        self.pflux = pflux
+        self.pfluxe = pfluxe
+        self.iflux = iflux
+        self.ifluxe = ifluxe
+        self.keys = ['radec','ra','rae','dec','dece','pflux','pfluxe']
+        if iflux:
+            self.keys.append('iflux')
+            self.keys.append('ifluxe')
+
+# fucntion to call whether to check tgss or vssr
+def ReadData(database):
+
+    if database == "tgss":
+        data = pd.read_csv("tgsscutout.csv")
+        dataset = datawrapp("tgss",np.array(data.RA),np.array(data.e_RA),\
+        np.array(data.DEC),np.array(data.e_DEC),np.array(data.Spk/1000),\
+        np.array(data.e_Spk/1000),np.array(data.Sint/1000),\
+        np.array(data.e_Sint/1000))
+
+    if database == "vssr":
+        data = pd.read_excel("vssrdataset.xls",sheet_name = "vlssr")
+        dataset = datawrapp("vssr",np.array(data.ra),[],\
+        np.array(data.dec),[],np.array(data.flux_74_mhz),\
+        np.array(data.flux_74_mhz_error),[],[])
+    return dataset

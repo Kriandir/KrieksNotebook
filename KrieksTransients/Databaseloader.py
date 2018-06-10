b35618ab14a30ab89f32ae1fe6bca0f0baba3652
Kriandir <kriekvdmeulen@gmail.com>
2018-06-10 15:23:01 +0200

    added frequency to databaseloader

--- KrieksTransients/Databaseloader.py
+++ KrieksTransients/Databaseloader.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 # tiny class wrapper around the tgssdata for ease of access
 class datawrapp(object):
-    def __init__(self,name,ra,rae,dec,dece,pflux,pfluxe,iflux,ifluxe):
+    def __init__(self,name,ra,rae,dec,dece,pflux,pfluxe,freq,iflux,ifluxe):
 
         self.radec = np.stack((ra,dec),axis = -1)
         self.ra = ra
@@ -18,9 +18,10 @@ class datawrapp(object):
             self.dece = dece
         self.pflux = pflux
         self.pfluxe = pfluxe
+        self.freq = freq
         self.iflux = iflux
         self.ifluxe = ifluxe
-        self.keys = ['radec','ra','rae','dec','dece','pflux','pfluxe']
+        self.keys = ['radec','ra','rae','dec','dece','pflux','pfluxe','freq']
         if iflux:
             self.keys.append('iflux')
             self.keys.append('ifluxe')
@@ -32,12 +33,12 @@ def ReadData(database):
         data = pd.read_csv("tgsscutout.csv")
         dataset = datawrapp("tgss",np.array(data.RA),np.array(data.e_RA),\
         np.array(data.DEC),np.array(data.e_DEC),np.array(data.Spk/1000),\
-        np.array(data.e_Spk/1000),np.array(data.Sint/1000),\
+        np.array(data.e_Spk/1000),147.5,np.array(data.Sint/1000),\
         np.array(data.e_Sint/1000))
 
     if database == "vssr":
         data = pd.read_excel("vssrdataset.xls",sheet_name = "vlssr")
         dataset = datawrapp("vssr",np.array(data.ra),[],\
         np.array(data.dec),[],np.array(data.flux_74_mhz),\
-        np.array(data.flux_74_mhz_error),[],[])
+        np.array(data.flux_74_mhz_error),74,[],[])
     return dataset

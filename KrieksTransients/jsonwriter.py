a6917a972b3c4139e60840967a74c66e29415505
Kriandir <kriekvdmeulen@gmail.com>
2018-09-19 17:44:42 +0200

    updated jsonwriter

--- KrieksTransients/jsonwriter.py
+++ KrieksTransients/jsonwriter.py
@@ -11,8 +11,13 @@ import json
 
 
 def WriteFluxJson(sources):
+    sourcelist = []
+    for i in sources:
+        data = {'url':"http://vizier.u-strasbg.fr/viz-bin/VizieR-3?-source=VIII/85A/spectra&-c="+"%0.4f"+"%2B"+"%0.4f"+"&-c.u=arcsec&-c.r=5&-out.add=_r&-sort=_r" %(i.ra,i.dec),'id':i.id}
+        sourcelist.append(data)
     print sources.ra
     print sources.dec
+    print sourcelist
 # multikeys = []
 # data = {'url':"http://vizier.u-strasbg.fr/viz-bin/VizieR-3?-source=VIII/85A/spectra&-c=185.7163%2B50.4487&-c.u=arcsec&-c.r=5&-out.add=_r&-sort=_r",'id':2312}
 # multikeys.append(data)

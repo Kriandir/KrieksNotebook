b00d5f67d6fc36dd092e56b118f4b730c3e3fa2b
Kriandir <kriekvdmeulen@gmail.com>
2018-09-19 19:01:25 +0200

    updated jsonwriter

--- KrieksTransients/jsonwriter.py
+++ KrieksTransients/jsonwriter.py
@@ -1,30 +1,15 @@
-# import json
-
-# data = {'url':"http://vizier.u-strasbg.fr/viz-bin/VizieR-3?-source=VIII/85A/spectra&-c=185.7163%2B50.4487&-c.u=arcsec&-c.r=5&-out.add=_r&-sort=_r",'id':2312}
-#
-# data.update({'url':"http://vizier.u-strasbg.fr/viz-bin/VizieR-3?-source=VIII/85A/spectra&-c=182.7163%2B50.4487&-c.u=arcsec&-c.r=5&-out.add=_r&-sort=_r",'id':1243})
-# # print data
-# # with open('data.json', 'w') as outfile:
-#
-
 import json
 
 
 def WriteFluxJson(sources):
     sourcelist = []
     for i in sources:
-        data = {'url':"http://vizier.u-strasbg.fr/viz-bin/VizieR-3?-source=VIII/85A/spectra&-c="+"%0.4f"+"%2B"+"%0.4f"+"&-c.u=arcsec&-c.r=5&-out.add=_r&-sort=_r" %(i.ra,i.dec),'id':i.id}
+        if i.ra < 0:
+            ra = i.ra+360
+            ra = "%0.4f" % ra
+	ra = "%0.4f" % i.ra
+	dec = "%0.4f" %i.dec
+        data = {'url':"http://vizier.u-strasbg.fr/viz-bin/VizieR-3?-source=VIII/85A/spectra&-c="+ra+"%2B"+dec+"&-c.u=arcsec&-c.r=5&-out.add=_r&-sort=_r",'id':str(i.id)}
         sourcelist.append(data)
-    print sources.ra
-    print sources.dec
-    print sourcelist
-# multikeys = []
-# data = {'url':"http://vizier.u-strasbg.fr/viz-bin/VizieR-3?-source=VIII/85A/spectra&-c=185.7163%2B50.4487&-c.u=arcsec&-c.r=5&-out.add=_r&-sort=_r",'id':2312}
-# multikeys.append(data)
-# data = {'url':"http://vizier.u-strasbg.fr/viz-bin/VizieR-3?-source=VIII/85A/spectra&-c=182.7163%2B50.4487&-c.u=arcsec&-c.r=5&-out.add=_r&-sort=_r",'id':1243}
-# multikeys.append(data)
-# # for i in range(3):
-# #     multikeys.append(data)
-# print multikeys
-# with open('data.json', 'w') as outfile:
-#     json.dump(multikeys,outfile)
+    with open('fluxvar.json','w') as outfile:
+        json.dump(sourcelist,outfile)

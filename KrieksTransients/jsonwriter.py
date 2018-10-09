2bfe68a401066c3b3203693c72042ac8faf03db7
Kriandir <kriekvdmeulen@gmail.com>
2018-10-09 17:30:22 +0200

    wooop

--- KrieksTransients/jsonwriter.py
+++ KrieksTransients/jsonwriter.py
@@ -1,7 +1,7 @@
 import json
 
 
-def WriteFluxJson(sources,pos = False,tg = True):
+def WriteFluxJson(sources,pos = False):
     sourcelist = []
     if not pos:
         try:
@@ -13,23 +13,24 @@ def WriteFluxJson(sources,pos = False,tg = True):
             if i.ra < 0:
                 ra = i.ra+360
                 ra = "%0.4f" % ra
-        	ra = "%0.4f" % i.ra
-        	dec = "%0.4f" %i.dec
+            ra = "%0.4f" % i.ra
+            dec = "%0.4f" %i.dec
             data = {'url':"http://vizier.u-strasbg.fr/viz-bin/VizieR-3?-source=VIII/85A/spectra&-c="+ra+"%2B"+dec+"&-c.u=arcsec&-c.r=5&-out.add=_r&-sort=_r",'id':str(i.id)}
             sourcelist.append(data)
     else:
-        if tg:
+        if sources[0].dataname == "tgss":
             try:
                 os.remove('tgpos.json')
             except:
                 pass
             name = 'tgpos'
             for i in sources:
-            	ra = "%0.4f" % i.ra
-            	dec = "%0.4f" %i.dec
-                pos = ra + " " + dec
-                data = {'url':"https://vo.astron.nl/tgssadr/q_fits/cutout/form",'id':str(i.id),'pos':pos}
-                sourcelist.append(data)
+                if i.dataname == "tgss":
+            	    ra = "%0.4f" % i.ra
+            	    dec = "%0.4f" %i.dec
+                    pos = ra + " " + dec
+                    data = {'url':"https://vo.astron.nl/tgssadr/q_fits/cutout/form",'id':str(i.id),'pos':pos}
+                    sourcelist.append(data)
 
     with open(name + '.json','w') as outfile:
         json.dump(sourcelist,outfile)

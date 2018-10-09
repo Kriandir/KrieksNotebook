176a2762cec8e84786141161d1dec1ff93c9247a
Kriandir <kriekvdmeulen@gmail.com>
2018-10-09 16:18:11 +0200

    updated some things with reg

--- KrieksTransients/writereg.py
+++ KrieksTransients/writereg.py
@@ -13,12 +13,15 @@ def WriteReg(timescale,possources,fluxsources):
     f.write('# Region file format: DS9 version 4.1 \n')
     f.write('global color=green dashlist=8 3 width=1 font="helvetica 10 normal roman" select=1 highlite=1 dash=0 fixed=0 edit=1 move=1 delete=1 include=1 source=1\n')
     f.write('fk5\n')
-    for i in range(len(possources)):
+    for i in possources:
         d = i.c.to_string('hmsdms',sep=':',precision= 3).split(" ")
-        f.write('circle('+ d[0] +","+ d[1]+'\")')
+        f.write('circle('+ d[0] +","+ d[1]+',200\")\n')
+        f.write('circle('+ d[0] +","+ d[1]+',20\")\n')
 
     f.write('global color=red dashlist=8 3 width=1 font="helvetica 10 normal roman" select=1 highlite=1 dash=0 fixed=0 edit=1 move=1 delete=1 include=1 source=1\n')
     f.write('fk5\n')
-    for i in range(len(fluxsources)):
+    for i in fluxsources:
         d = i.c.to_string('hmsdms',sep=':',precision= 3).split(" ")
-        f.write('circle('+ d[0] +","+ d[1]+'\")')
+        f.write('circle('+ d[0] +","+ d[1]+',200\")\n')
+        f.write('circle('+ d[0] +","+ d[1]+',20\")\n')
+

5c6c696ce964a10fb0416ad4cf351552f2f8c42a
Kriandir <kriekvdmeulen@gmail.com>
2018-10-09 16:17:33 +0200

    fixed additionaly usability to jsonwriter

--- KrieksTransients/jsonwriter.py
+++ KrieksTransients/jsonwriter.py
@@ -1,19 +1,35 @@
 import json
 
 
-def WriteFluxJson(sources):
-    try:
-        os.remove('fluxvar.json')
-    except:
-        pass
+def WriteFluxJson(sources,pos = False,tg = True):
     sourcelist = []
-    for i in sources:
-        if i.ra < 0:
-            ra = i.ra+360
-            ra = "%0.4f" % ra
-	ra = "%0.4f" % i.ra
-	dec = "%0.4f" %i.dec
-        data = {'url':"http://vizier.u-strasbg.fr/viz-bin/VizieR-3?-source=VIII/85A/spectra&-c="+ra+"%2B"+dec+"&-c.u=arcsec&-c.r=5&-out.add=_r&-sort=_r",'id':str(i.id)}
-        sourcelist.append(data)
-    with open('fluxvar.json','w') as outfile:
+    if not pos:
+        try:
+            os.remove('fluxvar.json')
+        except:
+            pass
+        name = 'fluxvar'
+        for i in sources:
+            if i.ra < 0:
+                ra = i.ra+360
+                ra = "%0.4f" % ra
+        	ra = "%0.4f" % i.ra
+        	dec = "%0.4f" %i.dec
+            data = {'url':"http://vizier.u-strasbg.fr/viz-bin/VizieR-3?-source=VIII/85A/spectra&-c="+ra+"%2B"+dec+"&-c.u=arcsec&-c.r=5&-out.add=_r&-sort=_r",'id':str(i.id)}
+            sourcelist.append(data)
+    else:
+        if tg:
+            try:
+                os.remove('tgpos.json')
+            except:
+                pass
+            name = 'tgpos'
+            for i in sources:
+            	ra = "%0.4f" % i.ra
+            	dec = "%0.4f" %i.dec
+                pos = ra + " " + dec
+                data = {'url':"https://vo.astron.nl/tgssadr/q_fits/cutout/form",'id':str(i.id),'pos':pos}
+                sourcelist.append(data)
+
+    with open(name + '.json','w') as outfile:
         json.dump(sourcelist,outfile)

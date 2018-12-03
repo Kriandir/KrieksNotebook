59e7b02e6978a0d96d4e33954213ae1ecc7cd27a
Kriandir <kriekvdmeulen@gmail.com>
2018-12-03 11:56:03 +0100

    updatedcutout2.py

--- KrieksTransients/cutout2.py
+++ KrieksTransients/cutout2.py
@@ -9,7 +9,7 @@ from astropy.wcs import WCS
 
 imagefile = "../IMAGES/December/26_8192_4asec_all_1hr/1hr_allband_autothresh-t0004-image-pb.fits"
 
-def PlotterFunction(image_data,c,wcss,ids,size,database = 'own'):
+def PlotterFunction(image_data,c,wcss,ids,size,database):
 
     # GET CUTOUT AND PLOT IT
     plt.figure(figsize = (10,10))
@@ -20,42 +20,42 @@ def PlotterFunction(image_data,c,wcss,ids,size,database = 'own'):
     plt.show()
 
 def GetCutout(imagefile,ra,dec,ids,pixels,database = 'own'):
+
+    # READ IN FITS FILE
     hdu_list = fits.open(imagefile)
-
     image_data = hdu_list[0].data
-    w= WCS(hdu_list[0].header)
+    w = WCS(hdu_list[0].header)
 
 
-    # GET LOCATION OF SOURCE
+    # GET LOCATION OF SOURCE AND SIZE OF PICTURE
     size = u.Quantity(pixels, u.pixel)
     c = SkyCoord(ra, dec, frame='fk5', unit='deg')
 
 
-    # DEFINE WCS COORDINATE SYSTEM
+    # DEFINE WCS COORDINATE SYSTEM FROM FITS FILE
     wcss = WCS(naxis=2)
     wcss.wcs.ctype = ['RA---SIN','DEC--SIN']
     wcss.wcs.crval = [w.wcs.crval[0],w.wcs.crval[1]]
     wcss.wcs.crpix = [ w.wcs.crpix[0],w.wcs.crpix[1]]
+
     try:
+        # FIRST TRY CDELT
         wcss.wcs.cdelt = [w.wcs.cdelt[0],w.wcs.cdelt[1]]
+
         # RESHAPE DATA IN PROPER IMAGE FORMAT
         shape = (image_data.shape)
         image_data = image_data.reshape((shape[2],shape[3]))
-        if database:
-            PlotterFunction(image_data,c,wcss,size,ids,database)
-        else:
-            PlotterFunction(image_data,c,wcss,size,ids)
-        # if database == 'vlsrr':
-            # size = u.Quantity((60,60),u.pixel)
+        PlotterFunction(image_data,c,wcss,size,ids,database)
+
     except:
         try:
             shape = (image_data.shape)
             image_data = image_data.reshape((shape[2],shape[3]))
             wcss.wcs.cd = [w.wcs.cd[0],w.wcs.cd[1]]
-            if database:
-                PlotterFunction(image_data,c,wcss,size,ids,database)
-            else:
-                PlotterFunction(image_data,c,wcss,size,ids)
+            PlotterFunction(image_data,c,wcss,size,ids,database)
+
         except:
-            print "something went fatally wrong check the script"
+            print "their is no cdelt and cd in the wcs coordinates of your fits file"
+            print "check the script"
+
     hdu_list.close()

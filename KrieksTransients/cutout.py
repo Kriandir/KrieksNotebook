9f8e164f59edf2a6341eff95a0ba40910e829f4c
Kriandir <kriekvdmeulen@gmail.com>
2018-12-10 15:54:05 +0100

    fixed cutout for now

--- KrieksTransients/cutout.py
+++ KrieksTransients/cutout.py
@@ -9,17 +9,7 @@ from astropy.wcs import WCS
 
 imagefile = "../IMAGES/December/26_8192_4asec_all_1hr/1hr_allband_autothresh-t0004-image-pb.fits"
 
-def PlotterFunction(image_data,c,wcss,ids,size,database = 'own'):
-
-    # GET CUTOUT AND PLOT IT
-    plt.figure(figsize = (10,10))
-    cutout1 = Cutout2D(image_data, c,size,wcs=wcss)
-    plt.imshow(cutout1.data, origin='lower')
-    plt.colorbar()
-    plt.savefig(str(ids)+"cutout_"+database+".png")
-    plt.show()
-
-def GetCutout(imagefile,ra,dec,ids,pixels,database = 'own'):
+def GetCutout(imagefile,ra,dec,id,database = 'own'):
     hdu_list = fits.open(imagefile)
 
     image_data = hdu_list[0].data
@@ -27,7 +17,7 @@ def GetCutout(imagefile,ra,dec,ids,pixels,database = 'own'):
 
 
     # GET LOCATION OF SOURCE
-    size = u.Quantity(pixels, u.pixel)
+    size = u.Quantity((120, 120), u.pixel)
     c = SkyCoord(ra, dec, frame='fk5', unit='deg')
 
 
@@ -36,26 +26,26 @@ def GetCutout(imagefile,ra,dec,ids,pixels,database = 'own'):
     wcss.wcs.ctype = ['RA---SIN','DEC--SIN']
     wcss.wcs.crval = [w.wcs.crval[0],w.wcs.crval[1]]
     wcss.wcs.crpix = [ w.wcs.crpix[0],w.wcs.crpix[1]]
-    try:
+
+    if database =='own' or database =='vlssr':
         wcss.wcs.cdelt = [w.wcs.cdelt[0],w.wcs.cdelt[1]]
         # RESHAPE DATA IN PROPER IMAGE FORMAT
         shape = (image_data.shape)
         image_data = image_data.reshape((shape[2],shape[3]))
-        if database:
-            PlotterFunction(image_data,c,wcss,size,ids,database)
-        else:
-            PlotterFunction(image_data,c,wcss,size,ids)
-        # if database == 'vlsrr':
-            # size = u.Quantity((60,60),u.pixel)
-    except:
-        try:
-            shape = (image_data.shape)
-            image_data = image_data.reshape((shape[2],shape[3]))
-            wcss.wcs.cd = [w.wcs.cd[0],w.wcs.cd[1]]
-            if database:
-                PlotterFunction(image_data,c,wcss,size,ids,database)
-            else:
-                PlotterFunction(image_data,c,wcss,size,ids)
-        except:
-            print "something went fatally wrong check the script"
+        if database == 'vlsrr':
+            size = u.Quantity((60,60),u.pixel)
+    elif database == 'tgss':
+        wcss.wcs.cd = [w.wcs.cd[0],w.wcs.cd[1]]
     hdu_list.close()
+
+
+
+
+
+    # GET CUTOUT AND PLOT IT
+    plt.figure(figsize = (10,10))
+    cutout1 = Cutout2D(image_data, c,size,wcs=wcss)
+    plt.imshow(cutout1.data, origin='lower')
+    plt.colorbar()
+    plt.savefig(str(id)+"cutout_"+database+".png")
+    plt.show()
